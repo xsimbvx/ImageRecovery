7aa387566816ce6edbdc147a804bb814754efc52
xsimbvx <kirilljeee@gmail.com>
2015-12-08 22:56:31 +0200

    Проект переведен на потоки

--- NonBlind/InverseFiltering.cs
+++ NonBlind/InverseFiltering.cs
@@ -1,41 +1,59 @@
 ﻿using ImageEditor;
 using System.Numerics;
-using ImageRecovery.Tools;
 using System.Drawing;
 
-
-namespace ImageRecovery.NonBlind
+namespace ImageRecovery
 {
     public class InverseFiltering
     {
         /// <summary>
         /// Инверсная фильтрация
         /// </summary>
-        /// <param name="filter">ядро искажения (PSF)</param>
+        /// <param name="sourceImage"> искаженное изображение</param>
+        /// <param name="filter"> оператор искажения PSF</param>
         /// <returns></returns>
-        public static Image Filtering(Image sourceImage, ConvolutionFilter filter, bool interpolation)
+        public static Image Filtering(Image sourceImage, ConvolutionFilter filter)
+        {
+            //перевод PSF в частотную область (OTF)
+            Complex[,] otf = OpticalTransferFunction.Psf2otf(filter);
+            //получение обратного PSF
+            for (int u = 0; u < otf.GetLength(0); u++)
+                for (int v = 0; v < otf.GetLength(1); v++)
+                {
+                    otf[u, v] = 1f / otf[u, v];
+                }
+            //перевод OTF в пространственную область (PSF)   
+            ConvolutionFilter cf = OpticalTransferFunction.Otf2psf(otf);
+            //свёртка с обратной PSF
+            Image result = cf.FastConvolution(sourceImage);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sourceImage"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+       /* public static Image Filtering(double[,,] sourceImage, ConvolutionFilter filter)
         {
             Complex[,] otf = OpticalTransferFunction.Psf2otf(filter);
             for (int u = 0; u < otf.GetLength(0); u++)
                 for (int v = 0; v < otf.GetLength(1); v++)
-                {      
-                    otf[u,v] = 1f / otf[u,v];
+                {
+                    otf[u, v] = 1f / otf[u, v];
                 }
-            ConvolutionFilter cf = OpticalTransferFunction.Otf2psf(otf);
             int filterSize = filter.filterMatrix.GetLength(0);       //размер PSF
             int filterHalfSize = (filterSize - 1) / 2 + 1;                //центр PSF
-            Image expandedImage = sourceImage.Expand(filterHalfSize);
-            Image image = expandedImage;
-            if(interpolation)
-            {
-                double[,,] expImageArray = OpticalTransferFunction.Interpolation(Converter.ToDoubleArray(expandedImage), expandedImage.Height, expandedImage.Width);
-                image = OpticalTransferFunction.FourierConvolution(expImageArray, cf);
-            }
-            else
-                image = OpticalTransferFunction.FourierConvolution(expandedImage, cf);
 
-            return image;
+            ConvolutionFilter cf = OpticalTransferFunction.Otf2psf(otf);
+
+            Image result = cf.FastConvolution(sourceImage);
+
+            return result;
         }
-      
+        */
+
     }
 }
--- NonBlind/TikhonovFiltering.cs
+++ NonBlind/TikhonovFiltering.cs
@@ -1,8 +1,8 @@
 ﻿using ImageEditor;
 using System.Numerics;
-using ImageRecovery.Tools;
 
-namespace ImageRecovery.NonBlind
+
+namespace ImageRecovery
 {
     public class TikhonovFiltering
     {
@@ -26,42 +26,42 @@ namespace ImageRecovery.NonBlind
             ///g - искаженное изображение
 
             Complex[,] otf = OpticalTransferFunction.Psf2otf(filter);
-           int height = otf.GetLength(0);                                              //строк
-           int width = otf.GetLength(1);                                              //столбцов
-           Complex gamma = Complex.Zero;                                        //
-           Complex[,] otfZ = new Complex[height, width];                                   //комплексно сопряженная матрица ядра
-           Complex[,] otf2 = new Complex[height, width];                                   //матрица = |h|^2
-           Complex[,] p = {{0, 1, 0,},                                          //лапласиан
+            int height = otf.GetLength(0);                                              //строк
+            int width = otf.GetLength(1);                                              //столбцов
+            Complex gamma = Complex.Zero;                                        //
+            Complex[,] otfZ = new Complex[height, width];                                   //комплексно сопряженная матрица ядра
+            Complex[,] otf2 = new Complex[height, width];                                   //матрица = |h|^2
+            Complex[,] p = {{0, 1, 0,},                                          //лапласиан
                            {1, -4, 1,},
                            {0, 1, 0,},};
-          p = Fourier.Transform(p); 
-           for (int u = 0; u < p.GetLength(0); u++)
-               for (int v = 0; v < p.GetLength(1); v++)
-                   p[u, v] = OpticalTransferFunction.ModPow(p[u, v]);
+            p = Fourier.Transform(p);
+            for (int u = 0; u < p.GetLength(0); u++)
+                for (int v = 0; v < p.GetLength(1); v++)
+                    p[u, v] = OpticalTransferFunction.ModPow(p[u, v]);
 
-           for (int u = 0; u < height; u++)
-               for (int v = 0; v < width; v++)
-                   otfZ[u, v] = Complex.Conjugate(otf[u, v]);
+            for (int u = 0; u < height; u++)
+                for (int v = 0; v < width; v++)
+                    otfZ[u, v] = Complex.Conjugate(otf[u, v]);
 
-           for (int u = 0; u < height; u++)
-               for (int v = 0; v < width; v++)
-                   otf2[u, v] = OpticalTransferFunction.ModPow(otf[u, v]);
+            for (int u = 0; u < height; u++)
+                for (int v = 0; v < width; v++)
+                    otf2[u, v] = OpticalTransferFunction.ModPow(otf[u, v]);
 
-           for (int u = 0; u < height; u++)
-               for (int v = 0; v < width; v++)
-                   p[u, v] = p[u, v] * gamma;
+            for (int u = 0; u < height; u++)
+                for (int v = 0; v < width; v++)
+                    p[u, v] = p[u, v] * gamma;
 
-           for (int u = 0; u < height; u++)
-               for (int v = 0; v < width; v++)
-                   otf2[u, v] = otf2[u, v] + p[u, v];
+            for (int u = 0; u < height; u++)
+                for (int v = 0; v < width; v++)
+                    otf2[u, v] = otf2[u, v] + p[u, v];
 
-           
-           for (int u = 0; u < height; u++)
-               for (int v = 0; v < width; v++)
-                   otf[u, v] = otfZ[u, v] / otf2[u, v];
 
-           ConvolutionFilter cf = OpticalTransferFunction.Otf2psf(otf);
-             
+            for (int u = 0; u < height; u++)
+                for (int v = 0; v < width; v++)
+                    otf[u, v] = otfZ[u, v] / otf2[u, v];
+
+            ConvolutionFilter cf = OpticalTransferFunction.Otf2psf(otf);
+
             return cf;
         }
     }
